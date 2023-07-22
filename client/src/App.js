a0f3d0cb548bd4b7d2d7efa4715fdfbfb8e22d96
root-codes <muriithidennis340@gmail.com>
2023-07-22 15:43:09 +0300

    update commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import {Route, Routes } from "react-router-dom";
+import {Login, Main } from "./containers"
 
 const App = () => {
   return (
-    <div className="text-blue-500 font-semibold">App</div>
+    <div className="w-screen min-h-screen h-auto flex flex-col items-center justify-center">
+      <Routes>
+          <Route path="/*" element={<Main />} />
+          <Route path="/Login" element={<Login />} />
+      </Routes>
+
+    </div>
   );
 }
 
