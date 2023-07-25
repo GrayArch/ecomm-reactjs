1fd5a265e6576dc1f2d2c01b944403c81c23dffe
root-codes <muriithidennis340@gmail.com>
2023-07-25 15:15:03 +0300

    frontend update

---
+++ client/src/assets/index.js
@@ -0,0 +1,3 @@
+export { default as LoignBg } from "./img/loginBg.jpg";
+export { default as Logo } from "./img/logo.png";
+export { default as Avatar } from "./img/";
--- client/src/containers/Login.jsx
+++ client/src/containers/Login.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
+import {LoginBg} from "../assets"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='w-screen h-screen relative overflow-hidden flex'>
+       
+       {/*the background image */}
+          <img
+          src={LoginBg}
+          className='w-full h-full object-cover absolute top-0 left-0'
+          alt=''
+          />
+
+    </div>
   )
 }
 
-export default Login
+export default Login;
